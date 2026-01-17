9a4e648fd8f37dd79a0b9c09d1a0ff37a92f0e92
siddharthabhatnagar <bhatnagarbrand@gmail.com>
2026-01-17 17:29:27 +0530

    Minor Changes

--- app/src/main/java/com/project/tipandsplitcalculator/MainActivity.kt
+++ app/src/main/java/com/project/tipandsplitcalculator/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.project.tipandsplitcalculator.navigation.NavGraph
 import com.project.tipandsplitcalculator.ui.theme.TipAndSplitCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +21,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TipAndSplitCalculatorTheme {
-               MainScreen()
+               val navHostController= rememberNavController()
+                NavGraph(navHostController)
             }
         }
     }
